26502212d296a2c3b4e6f941fa88e0cc7136cc15
Nicely123 <nsl1212528@163.com>
2022-12-14 13:13:06 +0800

    异常分类————业务异常、系统异常、其他异常,统一在表现层处理异常

--- src/main/java/com/annsl/controller/AdminController.java
+++ src/main/java/com/annsl/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.annsl.controller;
 
 import com.annsl.domain.Admin;
+import com.annsl.exception.BusinessException;
+import com.annsl.exception.SystemException;
 import com.annsl.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +30,16 @@ public class AdminController {
 
     @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id){
+
+        /*if (id == 1){
+            throw  new BusinessException(Code.BUSINESS_ERR, "业务异常测试");
+        }
+        try {
+            int i = 1/0;
+        }catch (Exception exception){
+            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR, "连接服务器超时，请稍后重试", exception);
+        }*/
+
         Admin admin = adminService.getById(id);
         Integer code = admin != null ? Code.GET_OK : Code.GET_ERR;
         String msg = admin != null ? "" : "数据查询失败，请重试！";
